40990390fca1d2605e42f8345507ce86f312487a
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-04-17 21:33:16 +0530

    feat🛠: add snakes and ladders positions

---
+++ src/config.js
@@ -0,0 +1,61 @@
+export const snakes = [
+    {
+        from: 16, to: 6
+    }, 
+    {
+        from: 47, to: 26
+    },
+    {
+        from: 49, to: 11
+    },
+
+    {
+        from: 56, to: 53
+    },
+    {
+        from: 64, to: 60
+    },
+    {
+        from: 87, to: 24
+    },
+    {
+        from: 93, to: 73
+    },
+    {
+        from: 95, to: 75
+    },
+    {
+        from: 98, to: 78
+    }
+]
+
+export const laders = [
+    {
+        from: 1, to: 38
+    }, 
+    {
+        from: 4, to: 14
+    },
+    {
+        from: 9, to: 31
+    },
+    {
+        from: 21, to: 42
+    },
+    {
+        from: 28, to: 84
+    },
+    
+    {
+        from: 36, to: 44
+    },
+    {
+        from: 51, to: 67
+    },
+    {
+        from: 71, to: 91
+    },
+    {
+        from: 80, to: 100
+    }
+]
